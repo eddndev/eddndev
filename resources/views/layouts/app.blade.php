3ca13a611dff3220ada8f7781496db64e83d593a
Edd en Dev <155781655+eddZaphkiel@users.noreply.github.com>
2025-08-17 01:52:36 -0600

    Pixel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,7 @@
     {{-- Directiva de Vite para cargar los assets (CSS y JS) --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <!-- Meta Pixel Code -->
+    <!-- Meta Pixel Code -->
 <script>
 !function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -32,11 +33,11 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1254611935956770');
+fbq('init', '948792990780126');
 fbq('track', 'PageView');
 </script>
 <noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1254611935956770&ev=PageView&noscript=1"
+src="https://www.facebook.com/tr?id=948792990780126&ev=PageView&noscript=1"
 /></noscript>
 <!-- End Meta Pixel Code -->
 </head>
