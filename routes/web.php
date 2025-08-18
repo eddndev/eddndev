feada58edb83181892d16c7053850c931132b309
Edd en Dev <155781655+eddZaphkiel@users.noreply.github.com>
2025-08-18 13:42:01 -0600

    Add demo

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('demo/maquinaria', function () {
+    return view('demo.maquinaria');
+});
